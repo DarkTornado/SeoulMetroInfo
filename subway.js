94be510173a12ab9fef67f191dec42c4f54b41a2
Dark Tornado <dt3141592@naver.com>
2023-01-20 22:35:54 +0900

    일단 파일 분리

---
+++ subway.js
@@ -0,0 +1,39 @@
+var axios = require('axios');
+const cheerio = require('cheerio');
+
+(async function() {
+    var url = 'https://smapp.seoulmetro.co.kr:58443/traininfo/traininfoUserMap.do';
+    var response = await axios.post(url, {
+        "params": {
+            'line': '1',
+            'isCb': 'N'
+        }
+    }, {
+        headers: {
+            'User-Agent': 'Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Mobile Safari/537.36'
+        }
+    });
+    var $ = cheerio.load(response.data);
+    var data = $('div.1line_metro').children('div');
+    var result = [];
+    for (var n = 0; n < data.length; n++) {
+        var datum = $(data[n]);
+        var value = datum.attr('title');
+        result.push(value);
+    }
+    var data = $('div.1line_korail').children('div');
+    var result = [];
+    for (var n = 0; n < data.length; n++) {
+        var datum = $(data[n]);
+        datum = datum.attr('title');
+        datum = datum.split(' ');
+        result.push({
+            trainNo: datum[0],
+            station: datum[2],
+            status: datum[3],
+            terminal: datum[4]
+        });
+    }
+    console.log(JSON.stringify(result, null, 4));
+})();
+
